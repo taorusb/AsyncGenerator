84adfc49f0cb7f9e7637d55bbd8667932bf6612d
taorusb <taorus@inbox.ru>
2021-02-26 18:15:42 +0300

    updated

--- src/com/asyncgenerator/Main.java
+++ src/com/asyncgenerator/Main.java
@@ -3,14 +3,23 @@ package com.asyncgenerator;
 import com.asyncgenerator.fizzbuzz.FizzBuzz;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Semaphore;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
 
-        FizzBuzz fizzBuzz = new FizzBuzz(15);
+        Semaphore semaphore = new Semaphore(1, true);
+        FizzBuzz fizzBuzz = new FizzBuzz(15, semaphore);
 
         IntStream.rangeClosed(1, fizzBuzz.getNum())
-                .forEach(x -> CompletableFuture.runAsync(() -> fizzBuzz.fizz(x)).join());
+                .forEach(num -> {
+                    try {
+                        semaphore.acquire();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    CompletableFuture.runAsync(() -> fizzBuzz.fizz(num));
+                });
     }
 }
--- src/com/asyncgenerator/fizzbuzz/FizzBuzz.java
+++ src/com/asyncgenerator/fizzbuzz/FizzBuzz.java
@@ -1,15 +1,18 @@
 package com.asyncgenerator.fizzbuzz;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Semaphore;
 
 public class FizzBuzz {
 
     private int num;
+    private final Semaphore semaphore;
 
-    public FizzBuzz(int num) {
+    public FizzBuzz(int num, Semaphore semaphore) {
         if (num <= 0) {
             throw new IllegalArgumentException("The number must not be less than or equal to zero.");
         }
+        this.semaphore = semaphore;
         this.num = num;
     }
 
@@ -18,31 +21,35 @@ public class FizzBuzz {
     }
 
     public void fizz(int n) {
+
         if (n % 3 == 0) {
             if (n % 5 == 0) {
-                CompletableFuture.runAsync(this::buzzFizz).join();
+                CompletableFuture.runAsync(this::buzzFizz);
                 return;
             }
             System.out.print("fizz, ");
+            semaphore.release();
             return;
         }
-        CompletableFuture.runAsync(() -> buzz(n)).join();
+        CompletableFuture.runAsync(() -> buzz(n));
     }
 
     public void buzz(int n) {
         if (n % 5 == 0) {
             System.out.print("buzz, ");
+            semaphore.release();
             return;
         }
-        CompletableFuture.runAsync(() -> num(n)).join();
+        CompletableFuture.runAsync(() -> num(n));
     }
 
     public void buzzFizz() {
         System.out.print("fizzbuzz, ");
+        semaphore.release();
     }
 
     public void num(int n) {
         System.out.print(n + ", ");
+        semaphore.release();
     }
 }
-
